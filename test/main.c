50ec7cba309bf39ae4f17163c91226c70e26e4b6
Trevor Vannoy <trevorvannoy@montana.edu>
2025-02-17 20:34:37 -0700

    create empty test project
    This will be used for hardware integration tests of the different
    modules.

---
+++ test/main.c
@@ -0,0 +1,25 @@
+#include <intrinsics.h>
+#include <msp430fr2355.h>
+#include <stdbool.h>
+
+void init()
+{
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+
+    // Disable the GPIO power-on default high-impedance mdoe to activate
+    // previously configure port settings
+    PM5CTL0 &= ~LOCKLPM5;
+
+    // Enable global interrupts
+    __enable_interrupt();
+}
+
+int main(void)
+{
+    init();
+
+    while (true)
+    {
+    }
+}
