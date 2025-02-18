8015704402f1b16f6af0028671740f32505795f2
Trevor Vannoy <trevorvannoy@montana.edu>
2025-02-17 21:02:27 -0700

    move public documentation into the header file

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -1,18 +1,3 @@
-/**
- * @file
- * @brief Heartbeat LED module
- *
- * This module creates a heartbeat LED using a Timer B module.
- * This module is a singleton because it is tied to a particular timer interrupt;
- * making the isr generic was not trivial... This would be easier in C++...
- *
- * This module relies on preprocessor defines to configure itself:
- * - HEARTBEAT_TIMER_BASE_ADDR: base address of the timer module to use
- * - HEARTBEAT_TIMER_VECTOR address of the timer modules interrupt vector
- * - HEARTBEAT_PORT_BASE_ADDR: base address of the GPIO port to use
- * - HEARTBEAT_PIN: bitmask for the pin to use (e.g., BIT0)
- */
-
 #include <msp430fr2355.h>
 
 #include "heartbeat.h"
@@ -23,15 +8,6 @@ static volatile uint16_t *TBxCCTL0 = (uint16_t *)(HEARTBEAT_TIMER_BASE_ADDR + OF
 static volatile uint16_t *PDIR = (uint16_t *)(HEARTBEAT_PORT_BASE_ADDR + OFS_P1DIR);
 static volatile uint16_t *POUT = (uint16_t *)(HEARTBEAT_PORT_BASE_ADDR + OFS_P1OUT);
 
-/**
- * Initialize a heartbeat LED
- *
- * This function relies upon various #defines being set, as documented in the
- * file-level documentation. A timer module is set to compare mode to create
- * the timer interrupt for the heartbeat LED.
- *
- * @param half_period_ms Half-period of the heartbeat LED in ms.
- */
 void heartbeat_init(const uint16_t half_period_ms)
 {
     // Disable the timer before configuring to avoid unexpected behavior
