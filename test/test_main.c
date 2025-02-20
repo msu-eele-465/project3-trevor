7f21c501fee5c9132978d21a16f9b3d1b9bc51da
Trevor Vannoy <trevorvannoy@montana.edu>
2025-02-19 21:45:46 -0700

    rename main test function

--- test/test_main.c
+++ test/test_main.c
@@ -2,6 +2,8 @@
 #include <msp430fr2355.h>
 #include <stdbool.h>
 
+#include "delay.h"
+
 void init()
 {
     // Stop watchdog timer
@@ -11,8 +13,11 @@ void init()
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
 
+    P1DIR |= BIT0;
+    P1OUT &= ~BIT0;
+
     // Enable global interrupts
-    __enable_interrupt();
+    // __enable_interrupt();
 }
 
 int main(void)
@@ -21,5 +26,9 @@ int main(void)
 
     while (true)
     {
+        // (__delay_cycles(500 * 1000 * 1));
+        // __delay_cycles(500ul * 1000ul * 1ul);
+        delay_ms(1000);
+        P1OUT ^= BIT0;
     }
 }
